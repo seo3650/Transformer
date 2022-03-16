dbe42d12c99c4196cc3c762fd14c422dd51b944a
seo3650 <seo3650@kaist.ac.kr>
2022-03-16 16:12:49 +0900

    Init

---
+++ transformer/Modules.py
@@ -0,0 +1,19 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+__author__ = "Yu-Hsiang Huang"
+
+class ScaledDotProductAttention(nn.Module):
+    ''' Scaled Dot-Product Attention '''
+
+    def __init__(self, temperature, attn_dropout=0.1):
+        super().__init__()
+        self.temperature = temperature
+        self.dropout = nn.Dropout(attn_dropout)
+
+    def forward(self, q, k, v, mask=None):
+        # TODO: Implement attention
+        raise NotImplementedError
+
+        return output, attn
